4045d6bbfdfbf9628ae752c10feecc6c75e30121
tayyabjamshaid <tayyabjamshaid78@gmail.com>
2022-07-18 00:22:41 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(89);
+  const style = {
+    maxHeight: "275px",
+    minHeight: "38px",
+    resize: "none",
+    padding: "9px",
+    boxSizing: "border-box",
+    fontSize: "25px",
+    margin: "0 auto",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <textarea
+        style={style}
+        placeholder="Tell us about your business"
+        onChange={(v) => setValue(v)}
+      />{" "}
     </div>
   );
 }
